77f84bf992238c1fbe0d54bf50e5dc21b400e81a
reactNoobie <reactnoobie@gmail.com>
2020-11-11 15:52:42 +0600

    Netlify test

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <div>
       <p>
         {format(addDays(new Date(), 1), 'EEEE d-MMM')} there will be{' '}
-        <strong>{results.length}</strong> Soumik 9
+        <strong>{results.length}</strong> Soumik 9 Netlify
       </p>
       <hr></hr>
       {results
